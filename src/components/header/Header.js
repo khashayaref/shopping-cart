2a7a5ab21c78205ccef4798559a728a0f4484765
khashayar <khashayaref2020@gmail.com>
2022-04-09 18:22:12 +0430

    create a project and add header components

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './header.scss'
+
+
+const Header = ({}) => {
+    return ( 
+        <header className="header">
+            <a href="/"> React Shopping Cart</a>
+        </header>
+     );
+}
+ 
+export default Header;
